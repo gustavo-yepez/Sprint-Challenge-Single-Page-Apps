51618558dfcfe3effd36de0dd6d61573f9ebb6bc
Gustavo Yepez <gustavo@Gustavos-MacBook-Pro.local>
2020-01-08 21:52:58 -0500

    working on it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
-
+import {Route} from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import Character from "./components/CharacterCard";
+import SearchForm from "./components/SearchForm";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <SearchForm />
+      <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/list" component={CharacterList} />
+      <Route path='/results/:id' component={Character} />
     </main>
   );
 }
